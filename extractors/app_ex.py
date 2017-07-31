4c578208739ce6f21b67fab48827342a875bcc68
YontiLevin <yontilevin@gmail.com>
2017-07-31 17:55:20 +0300

    template revision

--- extractors/app_ex.py
+++ extractors/app_ex.py
@@ -6,6 +6,7 @@ import os
 import requests
 from time import sleep
 from tqdm import tqdm
+from templates2 import *
 from templates import *
 
 
@@ -15,7 +16,7 @@ def classify_report_by_type(soup):
         res = soup.find_all('td', {"class": "clsTable"})[0].contents[1].contents[1].contents[0]
     except:
         res = ''
-    print(res)
+    # print(res)
     return res
 
 
@@ -27,47 +28,62 @@ def get_csv_data(argv):
     return df
 
 forms = set()
-year = '2004'
-for data in tqdm(get_csv_data(['', 'appointment', year]).iterrows()):
-    url = data[1].html_link
-    company_name = data[1].company_name
-    print(url)
-    if url == 'nan':
-        continue
-    if type(url) != str:
-        continue
-    if len(url) < 10:
-        continue
-    res = requests.get(url)
+for year in range(2004, 2017):
+    year = str(year)
+    print ('\n{}'.format(year))
+    for x, data in tqdm(enumerate(get_csv_data(['', 'appointment', year]).iterrows())):
+        if x > 10:
+            break
+        sleep(0.5)
+        url = data[1].html_link
+        company_name = data[1].company_name
+        if url == 'nan':
+            continue
+        if type(url) != str:
+            continue
+        if len(url) < 10:
+            continue
+        res = requests.get(url)
 
-    if res.status_code != 200:
-        print(res.status_code)
+        if res.status_code != 200:
+            print(res.status_code)
 
-    soup = BeautifulSoup(res.content, 'html.parser')
+        page_text = res.content
+        soup = BeautifulSoup(page_text, 'html.parser')
+        # soup = BeautifulSoup(res.content.decode('utf-8', 'ignore'), 'html.parser')
 
-    functions = {
-        u'ת090': lambda x: template_090(x),
-        u'ת091': lambda x: template_091(x),
-        u'ת093': lambda x: template_093(x),
-        u'ת304': lambda x: tempalte_304(x),
-        u'ת306': lambda x: tempalte_306(x),
-        u'ת307': lambda x: tempalte_307(x)}
+        functions = {
+            # u'ת090': lambda x: template_090(x),
+            # u'ת091': lambda x: template_091(x)
+            # u'ת093': lambda x: template_093(x)
+            #  u'ת304': lambda x: template_304(x) #,
+            u'ת306': lambda x: template_306(x)
+            # u'ת307': lambda x: tempalte_307(x)
+        }
 
-    report_type = classify_report_by_type(soup)
-    if report_type not in functions.keys():
-        print(report_type)
-        continue
-    if not len(report_type):
-        continue
-    data = functions[report_type]((soup, year))
+        report_type = classify_report_by_type(soup)
+        if report_type not in functions.keys():
+            # print(report_type)
+            continue
+        if not len(report_type): # or report_type != u'ת093':
+            continue
+        # data = functions[report_type]((soup, year))
+        try:
+            print(url)
+            data = type1((soup, year))
+            # flag = False
+            break
+        except:
+            # data = type2((soup, year))
+            print '00000'
+            break
 
+        # len_before  = forms.__len__()
+        # forms.add(report_type)
+        # if forms.__len__() != len_before:
+        #     print(report_type)
+        #     print(url)
+        # # print(soup.find_all('textarea',{'class':'TextareaDeploy'})[0].contents)
 
-    # len_before  = forms.__len__()
-    # forms.add(report_type)
-    # if forms.__len__() != len_before:
-    #     print(report_type)
-    #     print(url)
-    # # print(soup.find_all('textarea',{'class':'TextareaDeploy'})[0].contents)
-    sleep(0.5)
 
 print(set(forms))
--- extractors/templates.py
+++ extractors/templates.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 91, 93, 304, 306
 info needed :
@@ -10,7 +11,8 @@ job_start_date
 jobs in the last five years
     job  title, company name , duration
 """
-# -*- coding: utf-8 -*-
+from itertools import product
+
 
 def get_publish_date(html):
     table0 = html.find_all('table')[0]
@@ -37,11 +39,18 @@ def get_identifier_type(table, content_idx1, content_idx2):
     print identifier_type, identifier_num
     return identifier_type, identifier_num
 
+
 def get_birthday(html, table_num):
     table = html.find_all('table')[table_num]
     birthday = table.find_all('span')[1].contents[0]
     print birthday
     return birthday
+
+
+def get_citizenshop(table, content_index, span_index=1):
+    citizenship = table.contents[content_index].find_all('span')[span_index].contents[0]
+    print citizenship
+    return citizenship
 
 
 def get_job_details(html, table_num, start_ind, title_ind, prior_ind):
@@ -53,14 +62,65 @@ def get_job_details(html, table_num, start_ind, title_ind, prior_ind):
 
     try:
         job_title = table.contents[title_ind].find_all('span')[0].contents[0]
+        if job_title == u'אחר':
+            raise Exception
     except:
         job_title = table.contents[title_ind].find('textarea').contents[0]
+
     if prior_ind:
         prior_job = table.contents[prior_ind].find('span', {'id': 'Row0NewField21'}).contents[0]
     else:
         prior_job = '---'
     print job_title, job_start_at, prior_job
     return job_title, job_start_at, prior_job
+
+
+def get_academic_background(table):
+    academic_background = []
+    for content_index, table_index in product([23, 27, 31, 3], [1, 0]):
+        try:
+            tr = table.contents[content_index].find_all('table')[table_index].find_all('tr')
+            for item in tr:
+                spans = item.find_all('span')
+                txtareas = item.find_all('textarea')
+                aca_bk = [i.contents[0] for i in spans+txtareas]
+                academic_background.append(tuple(aca_bk))
+        except Exception:
+            pass
+        else:
+            break
+
+    for i in academic_background:
+        for j in i:
+            print j
+    return academic_background
+
+
+def get_prior_jobs(table, html=None):
+    prior_jobs_last_5 = []
+    for content_index, table_index in product([29, 31, 35, 3], [1, 0]):
+        try:
+            # prior_jobs_last_5 = [
+            #     (item.find_all('textarea')[0].contents[0], item.find_all('textarea')[1].contents[0],
+            #      item.find_all('textarea')[2].contents[0])
+            #     for item in table.contents[content_index].find_all('table')[table_index].find_all('tr')]
+            tr = table.contents[content_index].find_all('table')[table_index].find_all('tr')
+            for item in tr:
+                spans = item.find_all('span')
+                txtareas = item.find_all('textarea')
+                aca_bk = [i.contents[0] for i in txtareas+ spans]
+                prior_jobs_last_5.append(tuple(aca_bk))
+        except Exception:
+            pass
+        else:
+            break
+
+    for i in prior_jobs_last_5:
+        for j in i:
+            print j
+
+    return prior_jobs_last_5
+
 
 def template_091_type_a(html):
     publish_date  = get_publish_date(html)
@@ -69,28 +129,15 @@ def template_091_type_a(html):
 
     identifier_type, identifier_num = get_identifier_type(table, 5, 7)
 
-    citizenship = table.contents[9].find_all('span')[1].contents[0]
+    citizenship = get_citizenshop(table, content_index=9)
 
     birth_date = get_birthday(html, 5)
 
     job_title, job_start_at, prior_job_here = get_job_details(html, 4, 17, 21, 27)
 
+    academic_background = get_academic_background(table)
 
-    academic_background = [
-        (item.find_all('span')[0].contents[0], item.find_all('span')[1].contents[0], item.find('textarea').contents[0])
-        for item in table4.contents[31].find_all('table')[1].find_all('tr')]
-
-    for i in academic_background:
-        for j in i:
-            print j
-
-    prior_jobs_last_5 = [
-        (item.find_all('textarea')[0].contents[0], item.find_all('textarea')[1].contents[0], item.find_all('textarea')[2].contents[0])
-        for item in table4.contents[39].find_all('table')[1].find_all('tr')]
-
-    for i in prior_jobs_last_5:
-        for j in i:
-            print j
+    prior_jobs = get_prior_jobs(table)
 
 
 def template_091_type_b(html):
@@ -100,47 +147,16 @@ def template_091_type_b(html):
 
     identifier_type, identifier_num = get_identifier_type(table, 3 ,5)
 
-    citizenship = table.contents[7].find_all('span')[1].contents[0]
+    citizenship = get_citizenshop(table, content_index=7)
 
     birth_date = get_birthday(html, 5)
 
     job_title, job_start_at, prior_job_here = get_job_details(html, 3, 15, 19, 0)
 
-    try:
-        academic_background = [
-            (item.find_all('span')[0].contents[0], item.find_all('span')[1].contents[0], item.find('textarea').contents[0])
-            for item in table.contents[23].find_all('table')[1].find_all('tr')]
+    academic_background = get_academic_background(table)
 
-        for i in academic_background:
-            for j in i:
-                print j
+    prior_jobs = get_prior_jobs(table)
 
-        prior_jobs_last_5 = [
-            (item.find_all('textarea')[0].contents[0], item.find_all('textarea')[1].contents[0],
-             item.find_all('textarea')[2].contents[0])
-            for item in table.contents[31].find_all('table')[1].find_all('tr')]
-
-        for i in prior_jobs_last_5:
-            for j in i:
-                print j
-    except:
-        academic_background = [
-            (item.find_all('span')[0].contents[0], item.find_all('span')[1].contents[0],
-             item.find('textarea').contents[0])
-            for item in table.contents[27].find_all('table')[1].find_all('tr')]
-
-        for i in academic_background:
-            for j in i:
-                print j
-
-        prior_jobs_last_5 = [
-            (item.find_all('textarea')[0].contents[0], item.find_all('textarea')[1].contents[0],
-             item.find_all('textarea')[2].contents[0])
-            for item in table.contents[35].find_all('table')[1].find_all('tr')]
-
-        for i in prior_jobs_last_5:
-            for j in i:
-                print j
 
 def template_091(args):
     html, year = args
@@ -152,10 +168,32 @@ def template_091(args):
         pass
 
 
-def template_093(x):
-    pass
+def template_093(args):
+    html, year = args
+    publish_date = get_publish_date(html)
 
-def tempalte_304(args):
+    fullname, table = get_fullname(html, 3)
+
+    identifier_type, identifier_num = get_identifier_type(table, 1, 1)
+
+    citizenship = get_citizenshop(table, content_index=1, span_index=6)
+
+    table = html.find_all('table')[3]
+    birth_date = table.find_all('span')[10].contents[0]
+
+    job_title = table.find_all('span')[13].contents[0]
+    job_start_at = table.find_all('span')[25].contents[0]
+
+    print job_start_at, job_title
+
+    table = html.find_all('table')[9]
+    academic_background = get_academic_background(table)
+
+    table = html.find_all('table')[11]
+    prior_jobs = get_prior_jobs(table, html)
+
+
+def template_304(args):
     html, year = args
     publish_date = get_publish_date(html)
 
@@ -163,32 +201,55 @@ def tempalte_304(args):
 
     identifier_type, identifier_num = get_identifier_type(table, 1, 1)
 
-    citizenship = table.contents[1].find_all('span')[6].contents[0]
+    citizenship = get_citizenshop(table, content_index=1, span_index=6)
 
     birth_date = get_birthday(html, 12)
 
     job_title, job_start_at, prior_job_here = get_job_details(html, 3, 25, 11, 0)
 
+    table = html.find_all('table')[19]
+    academic_background = get_academic_background(table)
 
-    academic_background = [
-        (item.find_all('span')[0].contents[0], item.find_all('span')[1].contents[0], item.find('textarea').contents[0])
-        for item in table4.contents[31].find_all('table')[1].find_all('tr')]
-
-    for i in academic_background:
-        for j in i:
-            print j
-
-    prior_jobs_last_5 = [
-        (item.find_all('textarea')[0].contents[0], item.find_all('textarea')[1].contents[0], item.find_all('textarea')[2].contents[0])
-        for item in table4.contents[39].find_all('table')[1].find_all('tr')]
-
-    for i in prior_jobs_last_5:
-        for j in i:
-            print j
+    table = html.find_all('table')[21]
+    prior_jobs = get_prior_jobs(table, html)
 
 
-def tempalte_306(x):
-    pass
+def template_306(args):
+    html, year = args
+    publish_date = get_publish_date(html)
+
+    fullname, table = get_fullname(html, 5)
+    table = html.find_all('table')[6]
+    identifier_type = table.contents[1].find_all('span')[1].contents[0]
+    table = html.find_all('table')[7]
+    identifier_num = table.contents[1].find_all('span')[1].contents[0]
+
+    print  identifier_type, identifier_num
+    table = html.find_all('table')[8]
+    citizenship = get_citizenshop(table, content_index=1, span_index=1)
+
+    table = html.find_all('table')[9]
+    birthday = table.find_all('span')[1].contents[0]
+    birth_date = get_birthday(html, 10)
+
+    table = html.find_all('table')[12]
+    job_start_at = table.contents[1].find_all('span')[1].contents[0]
+
+    table = html.find_all('table')[15]
+    try:
+        job_title = table.contents[1].find_all('span')[0].contents[0]
+        if job_title == u'אחר':
+            raise Exception
+    except:
+        job_title = table.contents[1].find('textarea').contents[0]
+
+    print job_start_at, job_title
+
+    table = html.find_all('table')[16]
+    academic_background = get_academic_background(table)
+
+    table = html.find_all('table')[18]
+    prior_jobs = get_prior_jobs(table, html)
 
 """
 90, 307
@@ -201,8 +262,28 @@ job_start_date
 """
 
 
-def template_090(x):
-    pass
-def tempalte_307(x):
-    pass
+def template_090(args):
+    html, year = args
+    publish_date = get_publish_date(html)
+
+    fullname, table = get_fullname(html, 3)
+
+    job_title = table.contents[1].find_all('span')[0].contents[0].replace(':', '').replace('1.', '')[3:]
+
+    job_start_at = table.contents[3].find_all('span')[1].contents[0]
+
+    print job_title, job_start_at
+
+
+def tempalte_307(args):
+    html, year = args
+    publish_date = get_publish_date(html)
+
+    fullname, table = get_fullname(html, 4)
+
+    job_title = table.contents[1].find_all('span')[0].contents[0].replace(':', '').replace('1.', '')[3:]
+    table = html.find_all('table')[5]
+    job_start_at = table.contents[1].find_all('span')[1].contents[0]
+
+    print job_title, job_start_at
 
